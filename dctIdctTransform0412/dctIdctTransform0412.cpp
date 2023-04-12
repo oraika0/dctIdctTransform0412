93b187334e311242f93b7f7744f0fff964618510
oraika0 <kai91131@gmail.com>
2023-04-12 17:22:32 +0800

    vector debugging

--- dctIdctTransform0412/dctIdctTransform0412.cpp
+++ dctIdctTransform0412/dctIdctTransform0412.cpp
@@ -9,6 +9,7 @@ void getData(vector<vector<int>> pic);
 void dct_2d(vector<vector<int>> pic, vector<vector<int>> picDct);
 void idct_2d(vector<vector<int>> picDct, vector<vector<int>> picDctIdct);
 double C(int i);
+void print(vector<vector<int>> input);
 
 int main()
 {
@@ -18,43 +19,64 @@ int main()
 	getData(pic);
 	dct_2d(pic, picDct);
 	idct_2d(picDct, picDctIdct);
+	cout << "original : " << endl;
+	print(pic);
+	cout << "after dct : " << endl;
+	print(picDct);
+	cout << "idct: " << endl;
+	print(picDctIdct);
 
 }
 
 void getData(vector<vector<int>> pic){
-	int length;
-	cout << "picture's length(lenght should be equal to height to calculate) : ";
+	int length,width;
+	cout << "picture's length : ";
 	cin >> length;
+	cout << "picture's width : ";
+	cin >> width;
 	cout << "Enter your data from left to right , top to down";
 	for (int i = 0; i < length; i++)
 	{
-		for (int j = 0; i < length; j++)
+		cout << "row " << i + 1 << " : ";
+		for (int j = 0; j < width; j++)
 		{
 			cin >> pic[i][j];
 		}
+		cout << endl;
 	}
 	cout << "(input conplete)";
 }
 
 void dct_2d(vector<vector<int>> pic , vector<vector<int>> picDct) {
-	for (int i = 0; i < pic.size() ; i++)
-	{
-		for (int j = 0; j < pic.size(); j++) {
+	int M = pic[0].size();
+	int N = pic.size();
+	for (int u = 0; u < M ; u++){
+		for (int v = 0; v < N ; v++) {
 			double sigmaCal = 0;
-			for (int sigmaX = 0; sigmaX < pic.size() ; sigmaX++)
-			{
-				for (int sigmaY = 0; sigmaY < pic.size(); sigmaY++)
-				{
-					sigmaCal += pic[i][j] * cos(((2 * sigmaX + 1) * i * pi)/2 * pic.size()) * cos(((2 * sigmaY + 1) * j * pi) / 2 * pic.size());
+			for (int sigmaI = 0; sigmaI < M ; sigmaI++){
+				for (int sigmaJ = 0; sigmaJ < N ; sigmaJ++){
+					sigmaCal += pic[sigmaI][sigmaJ] * cos(((2 * sigmaI + 1) * u * pi)/(2 * M)) * cos(((2 * sigmaJ + 1) * v * pi) / 2 * N);
 				}
 			}
-			picDct[i][j] = 1 / 2 * sqrt(2 * pic.size()) * C(i) * C(j) * sigmaCal;
+			picDct[u][v] = (2 * C(u) * C(v)) / (sqrt(M * N)) *  sigmaCal;
 		}
 	}
 }
 
 void idct_2d(vector<vector<int>> picDct, vector<vector<int>> picDctIdct) {
-	;
+	int M = picDct[0].size();
+	int N = picDct.size();
+	for (int I = 0; I < M; I++) {
+		for (int J = 0; J < N; J++) {
+			double sigmaCal = 0;
+			for (int sigmaU = 0; sigmaU < M; sigmaU++) {
+				for (int sigmaV = 0; sigmaV < N; sigmaV++) {
+					sigmaCal += (2 * C(sigmaU) * C(sigmaV)) / (sqrt(M * N)) * picDct[sigmaU][sigmaV] * cos(((2 * sigmaU + 1) * I * pi) / (2 * M)) * cos(((2 * sigmaV + 1) * J * pi) / 2 * N);
+				}
+			}
+			picDctIdct[I][J] =  sigmaCal;
+		}
+	}
 }
 
 double C(int i) {
@@ -68,4 +90,14 @@ double C(int i) {
 	}
 }
 
+void print(vector<vector<int>> input) {
+	for (int i = 0; i < input[0].size() ; i++)
+	{
+		for (int j = 0; j < input.size(); j++) {
+			cout << input[i][j];
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
 
