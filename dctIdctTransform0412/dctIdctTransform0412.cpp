0ba700826d443cb65d8cf60651171baa5d50ccca
oraika0 <kai91131@gmail.com>
2023-04-13 21:23:31 +0800

    ok

--- dctIdctTransform0412/dctIdctTransform0412.cpp
+++ dctIdctTransform0412/dctIdctTransform0412.cpp
@@ -1,42 +1,40 @@
 ﻿#include <iostream>
 #include <math.h>
 #include <vector>
-
 #define pi 3.14159265f
 
 using namespace std;
 void getData(vector<vector<int>>& pic);
-void dct_2d(vector<vector<int>>& pic, vector<vector<double>>& picDct);
-void idct_2d(vector<vector<double>>& picDct, vector<vector<double>>& picDctIdct);
+void dct_2d(vector<vector<int>>& pic, vector<vector<int>>& picDct);
+void idct_2d(vector<vector<int>>& picDct, vector<vector<int>>& picDctIdct);
 double C(int i);
-void print(vector<vector<double>>& input);
 void print(vector<vector<int>>& input);
+void error(vector<vector<int>>& input1, vector<vector<int>>& input2);
 
 int main()
 {
 	vector<vector<int>> pic;
-	vector<vector<double>> picDct;
-	vector<vector<double>> picDctIdct;
+	vector<vector<int>> picDct;
+	vector<vector<int>> picDctIdct;
 	getData(pic);
-	print(pic);
 	dct_2d(pic, picDct);
 	idct_2d(picDct, picDctIdct);
 	cout << "original : " << endl;
-	//print(pic);
+	print(pic);
 	cout << "after dct : " << endl;
 	print(picDct);
 	cout << "idct: " << endl;
 	print(picDctIdct);
-
+	error(pic, picDctIdct);
 }
 
-void getData(vector<vector<int>>& pic){
-	int length,width;
+void getData(vector<vector<int>>& pic) {
+	int length, width;
 	cout << "picture's width : ";
 	cin >> width;
 	cout << "picture's length : ";
 	cin >> length;
-	
+
 	cout << "Enter your data from left to right , top to down" << endl;
 	for (int i = 0; i < length; i++)
 	{
@@ -54,72 +52,45 @@ void getData(vector<vector<int>>& pic){
 	cout << "(input complete)";
 }
 
-void dct_2d(vector<vector<int>>& pic , vector<vector<double>>& picDct) {
-	vector<int> NVector = pic[0]; 
-	int N = NVector.size();//width
-	int M = pic.size(); //height
+void dct_2d(vector<vector<int>>& pic, vector<vector<int>>& picDct) {
+	vector<int> NVector = pic[0];
+	int N = pic.size(); //width
+	int M = NVector.size();//column
 	//cout << N << " " << M;
-	for (int u = 0; u < M ; u++){
-		vector<double> tempVector;
-		for (int v = 0; v < N ; v++) {
-			double sigmaCalOut = 0;
-			for (int sigmaI = 0; sigmaI < M ; sigmaI++){
-				double sigmaCalIn = 0;
-				for (int sigmaJ = 0; sigmaJ < N ; sigmaJ++){
+	for (int u = 0; u < N; u++) {
+		vector<int> tempVector;
+		for (int v = 0; v < M; v++) {
+			double sigmaCal = 0;
+			for (int sigmaI = 0; sigmaI < N; sigmaI++) {
+				for (int sigmaJ = 0; sigmaJ < M; sigmaJ++) {
 
 
 					//cout << endl << sigmaJ << " ";
 					//cout << pic[sigmaI][sigmaJ] << endl;
-					sigmaCalIn += pic[sigmaI][sigmaJ] * cos(((2 * sigmaI + 1) * u * pi) / (2 * N)) * cos(((2 * sigmaJ + 1) * v * pi) / (2 * M));
+					sigmaCal += pic[sigmaI][sigmaJ] * cos(((2 * sigmaI + 1) * u * pi) / (2 * M)) * cos(((2 * sigmaJ + 1) * v * pi) / (2 * N));
+
 				}
-				sigmaCalOut += sigmaCalIn;
 			}
-			tempVector.push_back((2 * C(u) * C(v)) / (sqrt(M * N)) * sigmaCalOut);
+			tempVector.push_back((2 * C(u) * C(v)) / (sqrt(M * N)) * sigmaCal);
 		}
 		picDct.push_back(tempVector);
 	}
 }
 
-void idct_2d(vector<vector<double>>& picDct, vector<vector<double>>& picDctIdct) {
-	//vector<double> NVector = picDct[0];
-	//int N = NVector.size();//width
-	//int M = picDct.size(); //height
-	//for (int I = 0; I < M; I++) {
-	//	vector<double> tempVector;
-	//	for (int J = 0; J < N; J++) {
-	//		double sigmaCalOut = 0;
-	//		for (int sigmaU = 0; sigmaU < M; sigmaU++) {
-	//			double sigmaCalIn = 0;
-	//			for (int sigmaV = 0; sigmaV < N ; sigmaV++) {
-	//				sigmaCalIn += (2 * C(sigmaU) * C(sigmaV)) / (sqrt(M * N)) * picDct[sigmaU][sigmaV] * cos(((2 * sigmaU + 1) * I * pi) / (2 * N)) * cos(((2 * sigmaV + 1) * J * pi) / (2 * M));
-	//			}
-	//			sigmaCalOut += sigmaCalIn;
-	//		}
-	//		tempVector.push_back(sigmaCalOut);
-	//	}
-	//	picDctIdct.push_back(tempVector);
-	//}
-
-	vector<double> NVector = picDct[0];
-	int N = NVector.size();//width
-	int M = picDct.size(); //height
-	//cout << N << " " << M;
-	for (int u = 0; u < M; u++) {
-		vector<double> tempVector;
-		for (int v = 0; v < N; v++) {
-			double sigmaCalOut = 0;
-			for (int sigmaI = 0; sigmaI < M; sigmaI++) {
-				double sigmaCalIn = 0;
-				for (int sigmaJ = 0; sigmaJ < N; sigmaJ++) {
-
-
-					//cout << endl << sigmaJ << " ";
-					//cout << pic[sigmaI][sigmaJ] << endl;
-					sigmaCalIn += (((2 * C(sigmaI) * C(sigmaJ)) / (sqrt(M * N))) * picDct[sigmaI][sigmaJ] * cos(((2 * u + 1) * sigmaJ * pi) / (2 * N)) * cos(((2 * v + 1) * sigmaI * pi) / (2 * M)));
+void idct_2d(vector<vector<int>>& picDct, vector<vector<int>>& picDctIdct) {
+	vector<int> NVector = picDct[0];
+	int N = picDct.size(); //width
+	int M = NVector.size();//column
+	for (int m = 0; m < M; m++) {
+		vector<int> tempVector;
+		for (int n = 0; n < N; n++) {
+			double sigmaCal = 0;
+			for (int sigmaU = 0; sigmaU < N; sigmaU++) {
+				for (int sigmaV = 0; sigmaV < M; sigmaV++) {
+					sigmaCal += (2 * C(sigmaU) * C(sigmaV)) / (sqrt(M * N)) * picDct[sigmaU][sigmaV] * cos(((2 * m + 1) * sigmaU * pi) / (2 * M)) * cos(((2 * n + 1) * sigmaV * pi) / (2 * N));
 				}
-				sigmaCalOut += sigmaCalIn;
 			}
-			tempVector.push_back( sigmaCalOut);
+			tempVector.push_back(sigmaCal);
 		}
 		picDctIdct.push_back(tempVector);
 	}
@@ -136,18 +107,6 @@ double C(int i) {
 	}
 }
 
-void print(vector<vector<double>>& input) {
-	for (int i = 0; i < input.size() ; i++)
-	{
-		vector<double> tempVector;
-		tempVector = input[i];
-		for (int j = 0; j < input[0].size(); j++) {
-			cout << tempVector[j] << " " ;
-		}
-		cout << endl;
-	}
-	cout << endl;
-}
 void print(vector<vector<int>>& input) {
 	for (int i = 0; i < input.size(); i++)
 	{
@@ -158,6 +117,14 @@ void print(vector<vector<int>>& input) {
 		}
 		cout << endl;
 	}
-	cout << endl;
 }
-
+void error(vector<vector<int>>& input1, vector<vector<int>>& input2) {
+	double error = 0;
+	for (int i = 0; i < input1.size(); i++)
+	{
+		for (int j = 0; j < input1[0].size(); j++) {
+			error += abs((double(input1[i][j]) - input2[i][j]) / input2[i][j]);
+		}
+	}
+	cout << "error : " << error / input1.size() * input1[0].size() << "%";
+}
